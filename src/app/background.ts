47eb2428b9705d6591f5249d0a4e2d2b86991e4d
Max Stoiber <contact@mxstbr.com>
2021-04-10 20:15:15 +0200

    Cleanup code, add more detection methods

--- src/app/background.ts
+++ src/app/background.ts
@@ -5,35 +5,104 @@ const enc = new TextDecoder("utf-8");
 chrome.webRequest.onBeforeRequest.addListener(
   (details) => {
     try {
-      // Only check requests from the page itself, not any embeds
-      const initiator = psl.parse(new URL(details.initiator).hostname);
-      const url = psl.parse(new URL(details.url).hostname);
-
-      // @ts-ignore the TS types for the psl module are incorrect, .domain does exist.
-      if (initiator.domain !== url.domain) return;
-
-      // JSON body
-      if (!details.requestBody?.raw?.[0]?.bytes) return;
-
-      const body = enc
-        .decode(details.requestBody.raw[0].bytes)
-        .replace(/\s/g, "");
-
-      if (typeof body !== "string") return;
+      if (!isFirstPartyRequest(details)) return;
 
       const isGraphQL =
-        body.includes('"query":"{') ||
-        body.includes('"query":"query') ||
-        body.includes('"query":"mutation');
+        isJSONGraphQLBody(details) || isGraphQLQueryParam(details);
+      if (!isGraphQL) return;
 
-      if (isGraphQL) {
-        chrome.browserAction.setIcon({
-          tabId: details.tabId,
-          path: "/icons/graphql-true.png",
-        });
-      }
+      chrome.browserAction.setIcon({
+        tabId: details.tabId,
+        path: "/icons/graphql-true.png",
+      });
     } catch (err) {}
   },
   { urls: ["<all_urls>"] },
   ["requestBody"]
 );
+
+chrome.webRequest.onBeforeSendHeaders.addListener(
+  (details) => {
+    try {
+      const isGraphQL = isGraphQLContentType(details);
+      if (!isGraphQL) return;
+
+      chrome.browserAction.setIcon({
+        tabId: details.tabId,
+        path: "/icons/graphql-true.png",
+      });
+    } catch (err) {}
+  },
+  { urls: ["<all_urls>"] },
+  ["requestHeaders"]
+);
+
+/**
+ * Detect application/graphql GraphQL usage
+ */
+function isGraphQLContentType(
+  details: chrome.webRequest.WebRequestHeadersDetails
+) {
+  try {
+    const contentType =
+      details.requestHeaders.find(
+        ({ name }) => name.toLowerCase() === `content-type`
+      )?.value ?? "";
+
+    return contentType.indexOf(`graphql`) > -1;
+  } catch (err) {
+    return false;
+  }
+}
+
+/**
+ * Detect ?query={me{id}}-style GraphQL usage
+ */
+function isGraphQLQueryParam(details: chrome.webRequest.WebRequestDetails) {
+  try {
+    const params = new URL(details.url).searchParams;
+    const query = params.get("query") ?? "";
+    return (
+      // TODO: Just "{" might have too many false-positives?
+      query.indexOf(`{`) === 0 ||
+      query.indexOf(`query`) === 0 ||
+      query.indexOf(`mutation`) === 0
+    );
+  } catch (err) {
+    return false;
+  }
+}
+
+/**
+ * Detect JSON POST GraphQL usage
+ */
+function isJSONGraphQLBody(details: chrome.webRequest.WebRequestBodyDetails) {
+  try {
+    if (!details.requestBody?.raw?.[0]?.bytes) return false;
+
+    const body = enc
+      .decode(details.requestBody.raw[0].bytes)
+      .replace(/\s/g, "");
+
+    if (typeof body !== "string") return;
+
+    return (
+      body.includes('"query":"{') ||
+      body.includes('"query":"query') ||
+      body.includes('"query":"mutation')
+    );
+  } catch (err) {
+    return false;
+  }
+}
+
+/**
+ * To avoid false positives with embeds/iframes/other third-party scripts on a page we make sure the page itself sent the request
+ */
+function isFirstPartyRequest(details: chrome.webRequest.WebRequestDetails) {
+  const initiator = psl.parse(new URL(details.initiator).hostname);
+  const url = psl.parse(new URL(details.url).hostname);
+
+  // @ts-ignore the TS types for the psl module are incorrect, .domain does exist.
+  return initiator.domain === url.domain;
+}
